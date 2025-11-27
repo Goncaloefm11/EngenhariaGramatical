eb66d5c57b4f3b158a56a9c39d3e9b02dfbd7505
Goncaloefm11 <goncaloefmagalhaes@gmail.com>
2025-11-27 08:40:45 +0000

    tp3

---
+++ TP3/scope.py
@@ -0,0 +1,40 @@
+from lark import Visitor, Token
+
+class SymbolTable:
+    def __init__(self):
+        self.scopes = [{}] 
+    def enter_scope(self): self.scopes.append({})
+    def exit_scope(self): self.scopes.pop()
+    def declare(self, name): self.scopes[-1][name] = "var"
+    def lookup(self, name):
+        for scope in reversed(self.scopes):
+            if name in scope: return True
+        return False
+
+class ScopeAnalyser(Visitor):
+    def __init__(self):
+        self.table = SymbolTable()
+        self.errors = [] # Agora guarda dicionários: {'line': 1, 'message': '...'}
+
+    def block(self, tree): self.table.enter_scope()
+    
+    def assign_expr(self, tree):
+        var_token = tree.children[0]
+        if isinstance(var_token, Token): self.table.declare(var_token.value)
+
+    def var_access(self, tree):
+        var_token = tree.children[0]
+        name = var_token.value
+        if not self.table.lookup(name):
+            line = getattr(tree.meta, 'line', '?')
+            # MUDANÇA AQUI: Guardamos objeto estruturado
+            self.errors.append({
+                'line': line,
+                'message': f"Variável '{name}' não declarada.",
+                'severity': 'SCOPE'
+            })
+
+    def try_catch_stmt(self, tree):
+        if len(tree.children) == 4:
+            var_token = tree.children[2]
+            if isinstance(var_token, Token): self.table.declare(var_token.value)
